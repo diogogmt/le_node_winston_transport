4782f8349f54218fcd77ebce643d405c3447463b
Diogo Monteiro <diogo.gmt@gmail.com>
2015-05-07 16:26:22 -0400

    Updated default log levels for log entries logger to match the ones defined by the default winston transport.

--- index.js
+++ index.js
@@ -28,38 +28,58 @@
   /**
    * Winston Logentries transport
    *
-   * @description constructor for the Logentries transport
+   * @description constructor for the LogEntries transport
    *
    * @param {object}       options                 options for your Logentries transport
    * @param {string}       options.token           Required. Logentries destination token uuid
+   * @param {String}       options.name            Optional. Logentries transport name, defaults to logEntries
    * @param {Boolean}      options.secure          Optional. Use tls for communication, default is false
-   * @param {Object}       options.levels          Optional. Custom log levels, default is syslog-style
+   * @param {String}       options.level           Optional. Default log level, defaults to debug
+   * @param {Object}       options.levels          Optional. Custom log levels, defaults to winston default transport levels
    * @param {Boolean}      options.timestamp       Optional. Auto-generate a timestamp, default is true
    * @param {Boolean}      options.json            Optional. Log message and metadata as json object,
    *
    *
    * @type {Function}
    */
-  var Logentries = function (options) {
+  var LogEntries = function (options) {
     var self = this;
     options = options || {};
 
-    self.name = 'logentries';
-    self.level = options.level || 'info';
-    self.json = options.json === false ? false : true;
+    self.secure = options.secure === true;
+    self.timestamp = options.timestamp !== false;
+    self.level = options.level || 'debug';
+    // le_node does not log levels < 1
+    var levels = {
+      silly: 1,
+      debug: 2,
+      verbose: 3,
+      info: 4,
+      warn: 5,
+      error: 6
+    };
+    self.levels = options.levels || levels;
+    self.name = options.name || 'logEntries';
+    self.json = options.json !== false;
     if (!options.token) {
       throw new Error('Missing require parameter: token');
     }
 
     self.logger = le.logger({
-      token: options.token
+      token: options.token,
+      level: self.level,
+      levels: self.levels,
+      timestamp: self.timestamp,
+      secure: self.secure
     });
+
+    // TODO - decide what to do with the events emitted by le_node
   };
 
   // Inherit from `winston.Transport` to take advantage of the base functionality and `.handleExceptions()`.
-  util.inherits(Logentries, winston.Transport);
+  util.inherits(LogEntries, winston.Transport);
 
-  Logentries.prototype.log = function (level, msg, meta, callback) {
+  LogEntries.prototype.log = function (level, msg, meta, callback) {
     var self = this;
     var output = msg || '';
     if (self.json) {
@@ -74,5 +94,5 @@
   };
 
   // Define a getter so that `winston.transports.Logentries` is available and thus backwards compatible.
-  winston.transports.Logentries = Logentries;
+  winston.transports.LogEntries = LogEntries;
 }(module, require('util'), require('winston'), require('le_node'), require('underscore')));
